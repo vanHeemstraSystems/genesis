703b565b35b9a1bfb7703119b38e8ba56d723ced
Willem van Heemstra <wvanheemstra@icloud.com>
2024-12-30 15:47:32 +0000

    Update

--- containers/app/frontend/hello_genesis.py
+++ containers/app/frontend/hello_genesis.py
@@ -10,13 +10,13 @@ scene = gs.Scene(show_viewer=False) # use headless mode
 plane = scene.add_entity(
     gs.morphs.Plane(),
 )
-franka = scene.add_entity(
-    # gs.morphs.URDF(
-    #     file='urdf/panda_bullet/panda.urdf',
-    #     fixed=True,
-    # ),
-    gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"),
-)
+# franka = scene.add_entity(
+#     # gs.morphs.URDF(
+#     #     file='urdf/panda_bullet/panda.urdf',
+#     #     fixed=True,
+#     # ),
+#     gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"),
+# )
 
 scene.build()
 for i in range(1000):
